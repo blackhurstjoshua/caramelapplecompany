4d72e8af5ddb141aba3a54a38af49b9e4a5db3de
blackhurstjoshua <blackhurst.joshua@gmail.com>
2025-06-28 20:29:40 -0600

    supabase added to project

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="@sveltejs/kit" />
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
@@ -8,6 +10,17 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+}
+
+declare namespace App {
+	interface Locals {
+		supabase: import('@supabase/supabase-js').SupabaseClient
+	}
+}
+
+declare module '$env/static/public' {
+	export const PUBLIC_SUPABASE_URL: string
+	export const PUBLIC_SUPABASE_ANON_KEY: string
 }
 
 export {};
