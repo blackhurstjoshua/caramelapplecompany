b00b06d27f4c6955137db4fd5a2888ed9f299cd2
blackhurstjoshua <blackhurst.joshua@gmail.com>
2025-09-06 11:25:19 -0600

    Broken shit

--- src/lib/stores/flavors.ts
+++ src/lib/stores/flavors.ts
@@ -9,15 +9,15 @@ export interface Flavor {
   price: number;
 }
 // needs to pull products from supabase and cast them to Flavor
-import { getWeeklySpecials } from '$lib/services/products';
+// import { getWeeklySpecials } from '$lib/services/products';
 
-export const fetchWeeklyFlavors = async () => {
-  const products = await getWeeklySpecials();
-  return products.map(product => ({
-    ...product,
-    isWeeklySpecial: product.is_weekly_special
-  }));
-};
+// export const fetchWeeklyFlavors = async () => {
+//   const products = await getWeeklySpecials();
+//   return products.map(product => ({
+//     ...product,
+//     isWeeklySpecial: product.is_weekly_special
+//   }));
+// };
 
 export const weeklyFlavors = writable<Flavor[]>([
   {
